6071828de6bceb52507dbf90dfb00c304ff82ddf
zhorixx <150459763+zhorixx@users.noreply.github.com>
2024-02-27 16:25:20 +0300

    final version + pptx

--- ninja.py
+++ ninja.py
@@ -1,5 +1,4 @@
 import pygame
-import sys
 import os
 import random
 import sqlite3
@@ -23,7 +22,7 @@ WIDTH = 800
 HEIGHT = 600
 FPS = 11
 pygame.init()
-pygame.display.set_caption('Fruit-Ninja on Python')
+pygame.display.set_caption('Fruit Ninja on Python')
 pygame.mixer.music.load('sounds/misuc.mp3')
 pygame.mixer.music.play(-1)
 s = pygame.mixer.Sound('sounds/noj.wav')
@@ -39,11 +38,11 @@ BLUE = (0, 0, 255)
 
 
 font = pygame.font.Font(os.path.join(os.getcwd(), 'comic.ttf'), 42)
-score_text = font.render('Score : ' + str(score), True, (255, 255, 255))
+score_text = font.render('Points : ' + str(score), True, (255, 255, 255))
 lives_icon = pygame.image.load('images/white_lives.png')
 
 
-def generate_random_fruits(fruit):
+def randomka(fruit):
     fruit_path = "images/" + fruit + ".png"
     data[fruit] = {
         'img': pygame.image.load(fruit_path),
@@ -66,25 +65,25 @@ def generate_random_fruits(fruit):
 
 data = {}
 for fruit in fruits:
-    generate_random_fruits(fruit)
+    randomka(fruit)
 
 
-def hide_cross_lives(x, y):
+def poisk(x, y):
     gameDisplay.blit(pygame.image.load("images/red_lives.png"), (x, y))
 
 
 font_name = pygame.font.match_font('comic.ttf')
 
 
-def draw_text(display, text, size, x, y):
-    font = pygame.font.Font(font_name, size)
+def textowod(display, text, size, x, y):
+    font = pygame.font.Font('comic.ttf', size)
     text_surface = font.render(text, True, WHITE)
     text_rect = text_surface.get_rect()
     text_rect.midtop = (x, y)
     gameDisplay.blit(text_surface, text_rect)
 
 
-def draw_lives(display, x, y, lives, image):
+def zhizni(display, x, y, lives, image):
     for i in range(lives):
         img = pygame.image.load(image)
         img_rect = img.get_rect()
@@ -93,16 +92,15 @@ def draw_lives(display, x, y, lives, image):
         display.blit(img, img_rect)
 
 countt = 0
-def show_gameover_screen():
+def menu():
     global countt
-
     gameDisplay.blit(background, (0, 0))
-    draw_text(gameDisplay, "Fruit Ninja!", 90, WIDTH / 2, HEIGHT / 4)
+    textowod(gameDisplay, "Fruit Ninja on Python!", 50, WIDTH / 2, HEIGHT / 4 )
 
     if not game_over:
-        draw_text(gameDisplay, "Score : " + str(score), 50, WIDTH / 2, HEIGHT / 2)
-        draw_text(gameDisplay, "Top Score : " + str(topscore), 50, WIDTH / 2, HEIGHT / 2 + 50)
-    draw_text(gameDisplay, "Press a key to begin!", 64, WIDTH / 2, HEIGHT * 3 / 4)
+        textowod(gameDisplay, "Points : " + str(score), 50, WIDTH / 2, HEIGHT / 2 - 50)
+        textowod(gameDisplay, "Top Score : " + str(topscore), 50, WIDTH / 2, HEIGHT / 2 + 50)
+    textowod(gameDisplay, "Press a key to begin!", 50, WIDTH / 2, HEIGHT * 3 / 4)
     pygame.display.flip()
     waiting = True
     while waiting:
@@ -121,11 +119,11 @@ game_running = True
 while game_running:
     if game_over:
         if first_round:
-            show_gameover_screen()
+            menu()
             first_round = False
         game_over = False
         player_lives = 3
-        draw_lives(gameDisplay, 690, 5, player_lives, 'images/red_lives.png')
+        zhizni(gameDisplay, 690, 5, player_lives, 'images/red_lives.png')
         score = 0
 
     for event in pygame.event.get():
@@ -139,7 +137,7 @@ while game_running:
     else:
         countt = 0
     gameDisplay.blit(score_text, (0, 0))
-    draw_lives(gameDisplay, 690, 5, player_lives, 'images/red_lives.png')
+    zhizni(gameDisplay, 690, 5, player_lives, 'images/red_lives.png')
 
     for key, value in data.items():
         if value['throw']:
@@ -153,7 +151,7 @@ while game_running:
                                  (value['x'], value['y']))
 
             else:
-                generate_random_fruits(key)
+                randomka(key)
 
             current_position = pygame.mouse.get_pos()
 
@@ -163,11 +161,11 @@ while game_running:
                     player_lives -= 1
                     if player_lives == 0:
 
-                        hide_cross_lives(690, 15)
+                        poisk(690, 15)
                     elif player_lives == 1:
-                        hide_cross_lives(725, 15)
+                        poisk(725, 15)
                     elif player_lives == 2:
-                        hide_cross_lives(760, 15)
+                        poisk(760, 15)
                     if player_lives == 0:
                         if score > topscore:
                             topscore = score
@@ -186,7 +184,7 @@ while game_running:
                             bebra = f""" INSERT INTO {stats} (round, score, topscore) VALUES({rounddd}, {score}, {topscore})"""
                             cursor.execute(bebra)
                         Table = False
-                        show_gameover_screen()
+                        menu()
                         FPS += 5
                         game_over = True
 
@@ -201,10 +199,10 @@ while game_running:
                 if key != 'bomb':
                     score += 1
                     s.play()
-                score_text = font.render('Score : ' + str(score), True, (255, 255, 255))
+                score_text = font.render('Points : ' + str(score), True, (255, 255, 255))
                 value['hit'] = True
         else:
-            generate_random_fruits(key)
+            randomka(key)
 
     pygame.display.update()
     clock.tick(FPS)
